d4699a41a3f5dd5b800049c2d0a658029e428c05
Christopher Espiritu <christopheredrian@gmail.com>
2016-07-14 19:18:46 +0800

    Delete unecessary files,fixed: navigation, scrollbar on body, and content not auto adjusting

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php header('Location: /index.html'); ?>
+
+<?php
+//  Only used for heroku live - it needs a backend language
+// This code will be deleted before passing the softcopy
+header('Location: /index.html'); ?>
